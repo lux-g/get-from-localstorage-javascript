6b4804ad0e5900d28eecb1c4e66cbe636aac0695
Marc Trojanowski <53346585+lux-g@users.noreply.github.com>
2020-12-28 11:06:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 let products = [
-    {
-        name: 'crooks',
-        price: 32,
-        tag: 'tshirt'
-    },
-    {
-        name: 'adidas',
-        price: 40,
-        tag: 'tshirt'
-    },
-    {
-        name: 'nike',
-        price: 100,
-        tag: 'sneakers'
-    },
+  {
+      name: 'crooks',
+      price: 32,
+      tag: 'tshirt'
+  },
+  {
+      name: 'adidas',
+      price: 40,
+      tag: 'tshirt'
+  },
+  {
+      name: 'nike',
+      price: 100,
+      tag: 'sneakers'
+  },
 ]
+
+const btn1 = document.querySelector('.btn1')
+btn1.addEventListener('click', getItems)
 
 localStorage.setItem('product', JSON.stringify(products))
 const values = JSON.parse(localStorage.getItem('product'))
 
-function getItems() {
-    values.forEach(item => {
-        const allProducts = document.createElement('div');
-        allProducts.innerHTML = `
-            <p>Brand: ${item.name}</p>
-            <p>${item.tag}</p>
-            <p>Price: ${item.price}</p>
-        `
-        document.querySelector('.container').append(allProducts)
-    })
-}
 
-const btn1 = document.querySelector('.btn1')
-btn1.addEventListener('click', getItems)
+function getItems() {
+  values.forEach(item => {
+      const allProducts = document.createElement('div');
+      allProducts.innerHTML = `
+          <p>Brand: ${item.name}</p>
+          <p>${item.tag}</p>
+          <p>Price: ${item.price}</p>
+      `
+      document.querySelector('.container').append(allProducts)
+  })
+}
